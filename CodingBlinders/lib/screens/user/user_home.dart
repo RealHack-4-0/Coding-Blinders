2dbe7fec91db655d5ea4fa45ef54b2cfd31d294a
dhananjayaaps <dhananjayaaps@gmail.com>
2023-03-11 21:32:03 +0530

    user home

--- CodingBlinders/lib/screens/user/user_home.dart
+++ CodingBlinders/lib/screens/user/user_home.dart
@@ -1,15 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 
-class UserHomePage extends StatefulWidget {
-  const UserHomePage({Key? key}) : super(key: key);
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<UserHomePage> createState() => _UserHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _UserHomePageState extends State<UserHomePage> {
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    ShowAppointmentWidget(),
+    HomeScreenWidget(),
+    MakeAppointmentWidget(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Navigation Bar"),
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Show',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Home Screen",
+        style: TextStyle(fontSize: 24.0),
+      ),
+    );
+  }
+}
+
+class MakeAppointmentWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Make Appointment Screen",
+        style: TextStyle(fontSize: 24.0),
+      ),
+    );
+  }
+}
+
+class ShowAppointmentWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Show Appointment Screen",
+        style: TextStyle(fontSize: 24.0),
+      ),
+    );
   }
 }
