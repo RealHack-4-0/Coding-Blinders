4d0014742bef012b246176957af19b2fc918dff4
Isuru Liyanage <isuruliyanageofficial@gmail.com>
2023-03-12 02:16:57 +0530

    home navigation final

--- CodingBlinders/lib/screens/user/user_home.dart
+++ CodingBlinders/lib/screens/user/user_home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'showAppoinment.dart';
 import 'addAppoinment.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:codingblinders/screens/signin.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -58,39 +56,82 @@ class HomeScreenWidget extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 20),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  height: 200,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      aspectRatio: 16 / 9,
-                      enlargeCenterPage: true,
-                      enlargeStrategy: CenterPageEnlargeStrategy.height,
-                    ),
-                    items: [
-                      Image.network('https://picsum.photos/id/1015/800/600'),
-                      Image.network('https://picsum.photos/id/1016/800/600'),
-                      Image.network('https://picsum.photos/id/1018/800/600'),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'That is a paragraph',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ],
+          SizedBox(height: 60,),
+          Text(
+            'Welcome to National Hospital',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue[900],
+              fontWeight: FontWeight.bold,
             ),
           ),
+          Text(
+            'Centre of Excellence in Health Care',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          Container(
+            child: Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 16 / 9,
+                        enlargeCenterPage: true,
+                        viewportFraction: 2,
+                        enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      ),
+                      items: [
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-5.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-3.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-4.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-21.jpg'),
+                        // Image.network('https://cdn.hirunews.lk/Data/News_Images/202004/1587210106_650168_hirunews_Genaral-Hospital.jpg'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'About Us',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'The National Hospital of Sri Lanka situated in Colombo in a 32 acre block of land is the largest teaching hospital in Sri Lanka and the final referral centre in the country consisting of 3000 beds. it is the training centre for under graduates and post graduate trainees of the Faculty of Medicine. The nursing training school, Colombo, PBS, and Schools of Radiography, Pharmacy, Cardiograph, physiotherapy and occupational therapy are also affiliated to the National Hospital.',
+                          style: TextStyle(
+
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+
         ],
       ),
     );
+
   }
 }
 
@@ -104,17 +145,4 @@ class MakeAppointmentWidget extends StatelessWidget {
       ),
     );
   }
-}
-
-void signOut(BuildContext context) async {
-  await clearUserData();
-  Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(builder: (context) => SignInPage()),
-  );
-}
-
-Future<void> clearUserData() async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.clear();
 }
