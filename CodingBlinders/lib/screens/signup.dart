d89be43007892f008695ca1e184aefeaf3589212
Isuru Liyanage <isuruliyanageofficial@gmail.com>
2023-03-11 15:57:11 +0530

    signup ui only some validations

--- CodingBlinders/lib/screens/signup.dart
+++ CodingBlinders/lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
@@ -7,8 +8,169 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  DateTime? _birthday;
+  String? _selectedGender;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Full Name',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Address',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Telephone',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  border: OutlineInputBorder(),
+                ),
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Birthday',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.calendar_today),
+                    onPressed: _selectDate,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                controller: TextEditingController(
+                  text: _birthday == null
+                      ? ''
+                      : '${_birthday?.day}/${_birthday?.month}/${_birthday?.year}',
+                ),
+                onSaved: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    final parts = value.split('/');
+                    _birthday = DateTime(
+                      int.parse(parts[2]),
+                      int.parse(parts[1]),
+                      int.parse(parts[0]),
+                    );
+                  }
+                },
+              ),
+              SizedBox(height: 16.0),
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  labelText: 'Gender',
+                  border: OutlineInputBorder(),
+                ),
+                value: _selectedGender,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value;
+                  });
+                },
+                items: [
+                  DropdownMenuItem(
+                    child: Text('Male'),
+                    value: 'Male',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Female'),
+                    value: 'Female',
+                  ),
+                ],
+                onSaved: (value) {
+                  // Save the value of the field to your data model
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submit,
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _selectDate() async {
+    final selectedDate = await showDatePicker(
+      context: context,
+      initialDate: _birthday ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+
+    if (selectedDate != null) {
+      setState(() {
+        _birthday = selectedDate;
+      });
+    }
+  }
+
+  void _submit() {
+    final form = _formKey.currentState;
+    if (form != null && form.validate()) {
+      form.save();
+
+
+
+    }
   }
 }
