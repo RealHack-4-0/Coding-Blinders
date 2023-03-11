76e01a22ebfbd342ed3aa138f103dc7ec7b1b111
Isuru Liyanage <isuruliyanageofficial@gmail.com>
2023-03-12 02:04:45 +0530

    home navigation

--- CodingBlinders/lib/screens/user/user_home.dart
+++ CodingBlinders/lib/screens/user/user_home.dart
@@ -56,39 +56,63 @@ class HomeScreenWidget extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 20),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  height: 200,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      aspectRatio: 16 / 9,
-                      enlargeCenterPage: true,
-                      enlargeStrategy: CenterPageEnlargeStrategy.height,
+          SizedBox(height: 15,),
+          Text(
+            'Welcome to National Hospital',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue[900],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'Centre of Excellence in Health Care',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 15,),
+          Container(
+            child: Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 16 / 9,
+                        enlargeCenterPage: true,
+                        viewportFraction: 2,
+                        enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      ),
+                      items: [
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-5.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-3.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-4.jpg'),
+                        Image.network('http://www.nhsl.health.gov.lk/web/images/h-slide-21.jpg'),
+                        // Image.network('https://cdn.hirunews.lk/Data/News_Images/202004/1587210106_650168_hirunews_Genaral-Hospital.jpg'),
+                      ],
                     ),
-                    items: [
-                      Image.network('https://picsum.photos/id/1015/800/600'),
-                      Image.network('https://picsum.photos/id/1016/800/600'),
-                      Image.network('https://picsum.photos/id/1018/800/600'),
-                    ],
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'That is a paragraph',
-                    style: TextStyle(fontSize: 16),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'That is a paragraph',
+                      style: TextStyle(fontSize: 16),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
+
   }
 }
 
